8623241357dcc4f196c45fafee40df7f6a28856a
Hiago Cesar <hiagocesardacruz@gmail.com>
2026-05-09 11:54:13 -0300

    imc.py

---
+++ imc.py
@@ -0,0 +1,10 @@
+#Autor: Hiago Cesar
+#Entrada de imc
+
+nome = float(input'Digite seu nome:')
+peso = float(input('Digite seu peso:'))
+altura = float(input('Digite sua altura em metros:'))
+imc = peso / (altura ** 2)
+
+# exibindo o resultado
+print(f' seu imc é: {resultado:.2f}')
