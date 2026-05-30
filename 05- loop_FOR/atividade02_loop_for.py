ee5f2439a2938a7461b934838f35f5dfc7fbbf8b
Hiago Cesar <hiagocesardacruz@gmail.com>
2026-05-30 09:30:22 -0300

    loop_for

---
+++ 05- loop_FOR/atividade02_loop_for.py
@@ -0,0 +1,7 @@
+# Autor: Hiago Cesar
+# Projeto: Tabuada com Loop For
+
+numero = 2
+# loop FOR
+for i in range (1,11) :
+    print(f'{ numero} x {i} = { 1 * numero}')
---
+++ 05- loop_FOR/atividade03_loop_for.py
@@ -0,0 +1,10 @@
+# Autor: Hiago Cesar
+# Projeto: Tabuada com Loop For
+
+numero = 2
+# loop FOR
+
+numero = int(input('Digite a tabuada que deseja (exemplo 4):'))
+
+for i in range (1,11) :
+    print(f'{ numero} x {i} = { 1 * numero}')
