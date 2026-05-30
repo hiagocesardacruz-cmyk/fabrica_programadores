ea293d63d9c4c81ca668f35c540900a7cdc3ebf0
Hiago Cesar <hiagocesardacruz@gmail.com>
2026-05-30 10:20:17 -0300

    Loop for

---
+++ 05- loop_FOR/atividade04_loop_for.py
@@ -0,0 +1,12 @@
+# Autor: Nilton barros
+# Projeto: Loop For - variaveis de loop 
+
+numero = 2
+# loop FOR
+
+numero = int(input('Digite a tabuada que deseja :'))
+numero_inicio = int(intput(' Digite o inicio da tabuda :'))
+numero_fim = int(input('Digite o fim da tabuada:'))
+
+for i in range (numero_inicio, numero_fim + 1):
+    print(f'{ numero} x {i} = { 1 * numero}')
