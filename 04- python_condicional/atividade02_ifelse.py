132431e40ee5090bc6cf543444388a9bdc47628e
Hiago Cesar <hiagocesardacruz@gmail.com>
2026-08-15 10:31:34 -0300

    04- Condicional

---
+++ 04- python_condicional/atividade02_ifelse.py
@@ -0,0 +1,10 @@
+# Autor> Hiago Cesar
+# Projeto: Desvio Condicional
+
+# Criação das variáveis
+nome = input("Digite seu nome: ")
+idade = float(input("Digite sua idade: "))
+if idade >= 18:
+    print('Aluno maior de idade')
+else:
+    print('Aluno menor de idade')
