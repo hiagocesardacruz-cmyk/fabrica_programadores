3d70a0db7d4fdcdcb9f255c0103f3673c07e7458
Hiago Cesar <hiagocesardacruz@gmail.com>
2026-08-15 10:30:45 -0300

    Delete 04- python_condicional directory

--- 04- python_condicional/04- python_condicional/atividade06.py
+++
@@ -1,8 +0,0 @@
-# autor > Hiago Cesar
-# Projeto: Desvio Condicional
-
-trabalho = input("Você tem um trabalho? (sim/não): ")
-if trabalho == "sim":
-    print("Bom trabalho!")
-else:
-    print("Tenha um bom dia!")
